99482dc8f8e12550664925749efcfbfb0f38d73e
Aaron Long <aaron.long.402@gmail.com>
2023-07-11 14:52:56 +0700

    Initial Commit

---
+++ TheMovieDB/TheMovieDBApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheMovieDBApp.swift
+//  TheMovieDB
+//
+//  Created by Intern on 11/07/23.
+//
+
+import SwiftUI
+
+@main
+struct TheMovieDBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
